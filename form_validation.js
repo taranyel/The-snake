79154659d210843571feb11907796baf7bf289e9
Yelyzaveta Taranova <taranyel@fit.cvut.cz>
2024-04-21 14:21:31 +0200

    add menu buttons

--- form_validation.js
+++ form_validation.js
@@ -1,12 +1,12 @@
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const submit = document.getElementById("submit");
-const bad_username = document.getElementById("bad_username");
-const bad_password = document.getElementById("bad_password");
+const username = document.querySelector(".username");
+const password = document.querySelector(".password");
+const submit = document.querySelector(".submit");
+const bad_username = document.querySelector(".bad_username");
+const bad_password = document.querySelector(".bad_password");
 bad_username.textContent = "";
 bad_password.textContent = "";
 
-function validation() {
+function validateUsername() {
     if (!username.value) {
         bad_username.textContent = "You must fill this field!";
     } else {
@@ -14,9 +14,14 @@ function validation() {
             bad_username.textContent = "Must contain at least 4 characters!"
         } else if (username.value.length > 10) {
             bad_username.textContent = "Maximum 10 characters are allowed!"
+        } else {
+            return true;
         }
     }
+    return false;
+}
 
+function validatePassword() {
     if (!password.value) {
         bad_password.textContent = "You must fill this field!";
     } else {
@@ -29,4 +34,8 @@ function validation() {
         }
     }
     return false;
+}
+
+function validation() {
+    return validatePassword() && validateUsername();
 }
--- user.js
+++ user.js
@@ -20,6 +20,7 @@ class User {
 
 class Storage {
     user;
+    password;
 
     constructor() {
         this.user = new User();
@@ -27,7 +28,6 @@ class Storage {
 
     waitForLogin() {
         handleEvents.clearErrorsAndInputs();
-
         handleEvents.toggleSubmit(true);
         handleEvents.toggleButtons(true);
     }
@@ -45,10 +45,10 @@ class Storage {
         handleEvents.toggleButtons(false);
 
         if (!sessionStorage.getItem("logged_in")) {
-            handleEvents.showForm();
+            handleEvents.showLoginForm();
             handleEvents.toggleSubmit(false);
         } else {
-            handleEvents.hideForm();
+            handleEvents.hideLoginForm();
         }
     }
 
@@ -68,7 +68,7 @@ class Storage {
 
                 } else {
                     sessionStorage.setItem("logged_in", username_value);
-                    handleEvents.hideForm();
+                    handleEvents.hideLoginForm();
                 }
             }
         }
@@ -85,7 +85,7 @@ class Storage {
                 localStorage.setItem(this.user.username, JSON.stringify(this.user.createUser()));
 
                 sessionStorage.setItem("logged_in", this.user.username);
-                handleEvents.hideForm();
+                handleEvents.hideLoginForm();
             }
         }
     }
@@ -96,17 +96,31 @@ class Storage {
         this.waitForSignUp();
     }
 
-    changePassword(new_password) {
-        this.user.password = new_password;
+    changePassword() {
+        if (validatePassword()) {
+            const user = this.getLoggedInUser();
+            user.password = password.value;
+            localStorage.removeItem(user.username);
+            localStorage.setItem(user.username, JSON.stringify(user));
+        }
     }
 
-    changeUsername(new_username) {
-        if (!localStorage.getItem(new_username)) {
-            localStorage.removeItem(this.user.username);
-            this.user.username = new_username;
-            localStorage.setItem(this.user.username, JSON.stringify(this.user.createUser()));
-        } else {
+    getLoggedInUser() {
+        const username = sessionStorage.getItem("logged_in");
+        return JSON.parse(localStorage[username])
+    }
 
+    changeUsername() {
+        if (validateUsername()) {
+            if (!localStorage.getItem(username.value)) {
+                const user = this.getLoggedInUser();
+                const old_username = user.username;
+                user.username = username.value;
+                localStorage.removeItem(old_username);
+                localStorage.setItem(user.username, JSON.stringify(user));
+            } else {
+                handleEvents.fillError(bad_username, "Account is already exists!");
+            }
         }
     }
 }
@@ -115,36 +129,61 @@ class Storage {
 class HandleEvents {
 
     logout;
-    form;
+    login_form;
     nav;
     game_screen;
     menu_button;
+    change_username_button;
+    change_username_form;
+    change_password_button;
+    change_password_form;
+    submit_new_username;
+    submit_new_password;
     menu;
 
     constructor() {
         this.logout = document.getElementById("logout_button");
-        this.form = document.querySelector(".forms");
+        this.login_form = document.querySelector(".forms");
         this.nav = document.querySelector("nav");
         this.game_screen = document.querySelector(".game_block");
         this.menu_button = document.getElementById("menu_button");
         this.menu = document.getElementById("menu");
+        this.change_username_button = document.getElementById("change_username_button");
+        this.change_password_button = document.getElementById("change_password_button");
+        this.change_username_form = document.getElementById("change_username_form");
+        this.change_password_form = document.getElementById("change_password_form");
+        this.submit_new_username = document.getElementById("submit_new_username");
+        this.submit_new_password = document.getElementById("submit_new_password");
 
         log_in.addEventListener("click", storage.waitForLogin.bind(storage));
         sign_up.addEventListener("click", storage.waitForSignUp.bind(storage));
         this.logout.addEventListener("click", storage.logOut.bind(storage));
         this.menu_button.addEventListener("click", this.toggleMenu.bind(this));
+        this.change_username_button.addEventListener("click", this.showChangeUsernameForm.bind(this));
+        this.change_password_button.addEventListener("click", this.showChangePasswordForm.bind(this));
+        this.submit_new_username.addEventListener("click", storage.changeUsername.bind(storage));
+        this.submit_new_password.addEventListener("click", storage.changePassword.bind(storage));
     }
 
     fillError(field, message) {
         field.textContent = message;
     }
 
-    showForm() {
-        this.form.style.display = "flex";
+    showLoginForm() {
+        this.login_form.style.display = "flex";
     }
 
-    hideForm() {
-        this.form.style.display = "none";
+    showChangeUsernameForm() {
+
+        this.change_username_form.style.display = "flex";
+    }
+
+    showChangePasswordForm() {
+        this.change_password_form.style.display = "flex";
+    }
+
+    hideLoginForm() {
+        this.login_form.style.display = "none";
         this.nav.style.display = "flex";
         this.game_screen.style.display = "flex";
         this.logout.style.display = "flex";
